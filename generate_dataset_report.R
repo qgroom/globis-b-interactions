248d72d087e0f9bce28f8db452064468aea26791
jhpoelen <jhpoelen@xs4all.nl>
2018-09-10 09:44:35 -0700

    add csv

--- generate_dataset_report.R
+++ generate_dataset_report.R
@@ -6,4 +6,4 @@ query_string <- paste(query, sep= " ", collapse = " ")
 rglobi::query(query_string)
 datasets <- rglobi::query(query_string)
 write.table(datasets, file='namespace_datasets.tsv', sep='\t', row.names=F, quote=F)
-
+write.table(datasets, file='namespace_datasets.csv', sep='\t', row.names=F)
